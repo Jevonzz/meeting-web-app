11dbc042e6a04d9a945c3b6ec449ccb796467d46
Keman <keatvun191@gmail.com>
2024-04-27 20:31:09 +0800

    feat: added new images feat: added new sidebar etc.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const RootLayout = () => {
-  return <div>RootLayout</div>;
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
 };
 
 export default RootLayout;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    icon: "/icons/Home.svg",
+    path: "/",
+  },
+  {
+    label: "Upcoming",
+    icon: "/icons/upcoming.svg",
+    path: "/upcoming",
+  },
+  {
+    label: "Previous",
+    icon: "/icons/previous.svg",
+    path: "/previous",
+  },
+  {
+    label: "Recordings",
+    icon: "/icons/Video.svg",
+    path: "/recordings",
+  },
+  {
+    label: "Personal Room",
+    icon: "/icons/add-personal.svg",
+    path: "/personal-room",
+  },
+];
